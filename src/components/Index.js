db04df5f1592339a1f075d90010515db4119e8ee
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-07-19 18:02:12 -0400

    adds handle add functionality

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Foods from './Foods';
 
-export default function Index({ foods, handleDelete }) {
+export default function Index({ foods, handleDelete,handleAdd }) {
 	return (
 		<div>
+			<Form handleAdd={handleAdd}/>
 			<Foods foods={foods} handleDelete={ handleDelete }/>
 		</div>
 	);
