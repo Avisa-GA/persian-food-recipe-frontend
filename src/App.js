332efdd1ea5d29ef0bd605ce5b55ae5742aa9252
Avisa-SEI <aap571420@gmail.com>
2021-07-19 10:17:07 -0700

    wip: fetch data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const [foodsState, setFoodsState] = useState({ foods: [] });
+  const URL = "http://localhost:3000/foods";
+
+  useEffect(() => {
+    async function getFoods() {
+      try {
+        const foods = await fetch(URL)
+        .then(response => response.json());
+        console.log(foods);
+        setFoodsState({foods});
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getFoods();
+  }, []);
+
+  return <div className="App">
+    
+  </div>;
 }
 
 export default App;
