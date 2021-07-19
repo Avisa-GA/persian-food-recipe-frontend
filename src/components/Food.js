07c229ec70f3b981f66b85c84b5f467518e4a535
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-07-19 16:53:02 -0400

    sets up index iteration

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
-export default function Food() {
-	return <div>Food</div>;
+export default function Food({ food }) {
+	return (
+		<div className="container">
+			<img
+				src={food.img}
+				alt={food.title}
+				style={{ width: '300px', height: '300px' }}
+			/>
+			<h3>{food.title}</h3>
+			<ul>
+				{food.ingredients.map((ingredient, index) => (
+					<li key={index}>{ingredient}</li>
+				))}
+			</ul>
+			<div>
+				{food.directions.map((direction, index) => (
+					<p key={index}>{direction}</p>
+				))}
+			</div>
+		</div>
+	);
 }
