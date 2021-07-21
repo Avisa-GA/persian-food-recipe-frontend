cc01094df0aaf3eb3bf34c40877943c330251385
Avisa-SEI <aap571420@gmail.com>
2021-07-21 12:02:48 -0700

    wip: style

--- src/pages/Food.js
+++ src/pages/Food.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Food({ food, handleDelete, URL }) {
+export default function Food({ food, handleDelete, URL })
+{
 	return (
-		<div style={{marginLeft: "30%"}} className="row">
+		<div style={{ marginLeft: "30%" }} className="row">
 			<div className="col s12 m6 m7">
 				<div className="card medium">
 					<div className="card-image">
 						<img
-				            src={food.img}
-				            alt={food.title}
+							src={food.img}
+							alt={food.title}
 						/>
-					
-						<button style={{marginBottom: "10%", marginLeft: "10%"}} className="btn-floating halfway-fab waves-effect waves-light red" onClick={() => handleDelete(food.id)}><i className="material-icons">delete</i>
-			        </button>
+
+						<button style={{ marginBottom: "10%", marginLeft: "10%" }} className="btn-floating halfway-fab waves-effect waves-light red" onClick={() => handleDelete(food.id)}><i className="material-icons">delete</i>
+						</button>
 					</div>
 					<div className="card-content">
-                         <span style={{color: "#00796b", marginRight: "30%", fontSize: "14px", textAlign: "left"}} className="card-title">
+						<span style={{ color: "#00796b", marginRight: "30%", fontSize: "14px", textAlign: "left" }} className="card-title">
 							{food.title}
 						</span>
 					</div>
