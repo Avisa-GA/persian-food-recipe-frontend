13645dd02bef4aa2f1056ddc996c873878e4e966
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-07-21 15:10:48 -0400

    refactored show page toggle button

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -25,52 +25,51 @@ export default function Show({ foods, match, history, handleUpdate }) {
 			{toggleForm ? (
 				<Form food={food} handleUpdate={handleUpdate} />
 			) : (
-					<div className="row">
-						<div className="col s12 m6">
-							{food && (
-							<div style={{marginLeft: "20%", marginTop: "5%", width: "700px", height: "100%"}} className="card">
+				<div className="row">
+					<div className="col s12 m6">
+						{food && (
+							<div
+								style={{
+									marginLeft: '20%',
+									marginTop: '5%',
+									width: '700px',
+									height: '100%',
+								}}
+								className="card">
 								<div className="card-image">
-									
-										<>
-											<img
-								              src={food.img}
-								              alt={food.title}
-											/>
-											<span className="card-title">
-												{food.title}
-											</span>
-										</>
-									
-									</div>
-									<div className="card-content">
-										<StyledTitle style={{color: "#00796b"}}>Ingridients</StyledTitle>
-										{food.ingredients.map((ingredient, index) => (
-										
-									<p key={index}>{ingredient}</p>
-										))}
-
-										<StyledTitle style={{color: "#00796b"}}>Directions</StyledTitle>
-										{food.directions.map((direction, index) => (
-									<p key={index}>{direction}</p>
-								))}
-							
-									</div>
-									<div className="card-action">
-										<Link to="/">Go Back</Link>
-										<button className="waves-effect waves-light btn teal lighten-3" onClick={toggleOpenForm}>
-			                     {toggleForm ? 'Back to Recipe' : 'Edit Recipe'}
-	 		                           </button>
-									</div>
+									<>
+										<img src={food.img} alt={food.title} />
+										<span className="card-title">{food.title}</span>
+									</>
 								</div>
-								)
-								}
-						</div>
-						
+								<div className="card-content">
+									<StyledTitle style={{ color: '#00796b' }}>
+										Ingridients
+									</StyledTitle>
+									{food.ingredients.map((ingredient, index) => (
+										<p key={index}>{ingredient}</p>
+									))}
+
+									<StyledTitle style={{ color: '#00796b' }}>
+										Directions
+									</StyledTitle>
+									{food.directions.map((direction, index) => (
+										<p key={index}>{direction}</p>
+									))}
+								</div>
+							</div>
+						)}
+					</div>
 				</div>
 			)}
-			</>
-		
-	)
-			
-
+			<div className="card-action">
+				<Link to="/">Go Back</Link>
+				<button
+					className="waves-effect waves-light btn teal lighten-3"
+					onClick={toggleOpenForm}>
+					{toggleForm ? 'Back to Recipe' : 'Edit Recipe'}
+				</button>
+			</div>
+		</>
+	);
 }
